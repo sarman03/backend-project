aad70fb36ab46e28b65879acde62b34998cc6c95
sarman03 <sarmanpreets.it.22@nitj.ac.in>
2024-08-06 19:51:49 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app= express();
 
 ( async () => {
     try {
-        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+       app.on("error", (error)=>{
+        console.log("ERROR: ",error);
+        throw err;
+       })
+
+       app.listen(process.env.PORT, ()=>{
+        console.log(`App is listening on port ${process.env.PORT}`);
+       })
     } catch (error) {
         console.error("ERROR: ", error)
-        throw error
+        throw err;
     }
 } )()
+
+*/
