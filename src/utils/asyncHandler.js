2de4c95493b08c254de1c0d73e7e38d3fbcb9ae7
sarman03 <sarmanpreets.it.22@nitj.ac.in>
2024-08-07 18:14:33 +0530

    db connection

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,10 @@
-const asyncHandler= () => {}
+const asyncHandler= (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
+}
+
+
+
 
 export {asyncHandler}
