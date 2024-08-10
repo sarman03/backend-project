835c79adf3cdd40e704f7b42bf3345f07f9d0644
sarman03 <sarmanpreets.it.22@nitj.ac.in>
2024-08-10 14:25:22 +0530

    middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name: 'dhsyeryfv', 
+    api_key: '436899692178914', 
+    api_secret: 'lcP-hwAqfKqB6gRpYUxNrtboRw8'
+});
+
