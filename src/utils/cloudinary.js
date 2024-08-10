b37b313571f3a3fb8e0008f53ae9554d5f23bb78
sarman03 <sarmanpreets.it.22@nitj.ac.in>
2024-08-10 18:27:46 +0530

    routes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,8 +2,25 @@ import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
 cloudinary.config({ 
-    cloud_name: 'dhsyeryfv', 
-    api_key: '436899692178914', 
-    api_secret: 'lcP-hwAqfKqB6gRpYUxNrtboRw8'
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto",  
+        })
+        // upload hogi
+        console.log("file is uploaded on cloudinary", response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)  
+    }
+}
+
+
+export {uploadOnCloudinary}
