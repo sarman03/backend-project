2c4d9ccad25d738f78ca13eb32388627e4150eed
sarman03 <sarmanpreets.it.22@nitj.ac.in>
2024-08-12 23:20:10 +0530

    debug

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,8 +15,9 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto",  
         })
         // upload hogi
-        console.log("file is uploaded on cloudinary", response.url)
-        return response
+       // console.log("file is uploaded on cloudinary", response.url)
+       fs.unlinkSync(localFilePath)
+       return response
     } catch (error) {
         fs.unlinkSync(localFilePath)  
     }
